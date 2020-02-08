a1b6423f5fcc7f2912eff2da7b32aa769060585a
Telmo Sampaio <telmoasampaio@gmail.com>
2020-02-08 10:46:30 +0000

    Delete Profile & User

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -128,4 +128,21 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+//@route    DELETE api/profile
+//@desc     Delete profile, user & posts
+//@access   Private 
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove user posts
+        //Remove Profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        //Remove User
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User Deleted' });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
