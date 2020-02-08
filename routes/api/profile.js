bef817ee68291a09645f01de6a5c4fe52167c26d
Telmo Sampaio <telmoasampaio@gmail.com>
2020-02-08 11:11:18 +0000

    Add Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -145,4 +145,51 @@ router.delete('/', auth, async (req, res) => {
     }
 });
 
+//@route    PUT api/profile/experience
+//@desc     Add profile experience
+//@access   Private
+router.put('/experience', [auth, [ 
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From data is required').not().isEmpty(),
+] ], async (req, res) => {
+   const errors = validationResult(req);
+   if(!errors.isEmpty()) {
+       return res.status(400).json({ errors: errors.array() });
+   }
+
+   const {
+       title,
+       company,
+       location,
+       from,
+       to,
+       current,
+       description
+   } = req.body;
+
+   const newExp = {
+       title: title,
+       company,
+       location,
+       from,
+       to,
+       current,
+       description
+   }
+
+   try {
+       const profile = await Profile.findOne({ user: req.user.id });
+       
+       profile.experience.unshift(newExp);
+
+       await profile.save();
+
+       res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+   }
+});
+
 module.exports = router;
