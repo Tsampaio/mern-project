39ffd8eba17dc4c20ec85a30a81e585d6b09c105
Telmo Sampaio <telmoasampaio@gmail.com>
2020-02-08 11:35:55 +0000

    Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -192,4 +192,25 @@ router.put('/experience', [auth, [
    }
 });
 
+//@route    Delete api/profile/experience/:exp_id
+//@desc     Delete experience from profile
+//@access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) =>{
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        //Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+        
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
