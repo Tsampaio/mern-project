526132fa1ede9a08c0116dc24ab53644b9127582
Telmo Sampaio <telmoasampaio@gmail.com>
2020-02-09 17:02:40 +0000

    Starting Redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,12 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import './App.css';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => (
+  <Provider store={store}>
   <Router>
     <Fragment>
       <Navbar />
@@ -20,6 +24,7 @@ const App = () => (
       
     </Fragment>
   </Router>
+  </Provider>
 )
 
 export default App;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 
 const Register = () => {
     const [formData, setFormData] = useState({
