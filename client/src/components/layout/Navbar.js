34ed42fac7e98692d51375523827777afc3a0d81
Telmo Sampaio <telmoasampaio@gmail.com>
2020-02-21 22:41:32 +0000

    Profile Reducer & Get current Profile

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,10 +8,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li>
-            <a onClick={logout} href="#">
-                <i className="fas fa-sign-out-alt"></i>{' '}
-                <span className="hide-sm">Logout</span>
-            </a></li>
+                <a onClick={logout} href='#'>
+                    <i className="fas fa-sign-out-alt" />{' '}
+                    <span className="hide-sm">Logout</span>
+                </a>
+            </li>
         </ul>
     );
 
